ed693ea71bf5175b7c5c7470e6bffddf450fa4cf
sujaysp99 <sujayspofficial@gmail.com>
2025-03-08 19:09:15 -0600

    Creating UI

--- Code/GradioInterface.py
+++ Code/GradioInterface.py
@@ -4,26 +4,31 @@ import os
 import fitz
 from PIL import Image
 
-with gr.Blocks() as demo:
-    with gr.Row():
-        api_key = gr.Textbox(
-            placeholder='Enter OpenAI API key',
-            interactive=True,
-            label='API Key'
-        )
-        change_api_key = gr.Button('Change Key')
+def createUI():
+    with gr.Blocks() as demo:
+        with gr.Row():
+            api_key = gr.Textbox(
+                placeholder='Enter OpenAI API key',
+                interactive=True,
+                type="password",
+                label='API Key'
+            )
+            change_api_key = gr.Button('Change Key')
 
-    with gr.Row():
-        chatbot = gr.Chatbot(label='Chatbot', type='messages', height=650)
-        pdf_display = gr.Image(label='Uploaded PDF Page', interactive=False, height=680)
-    
-    with gr.Row():
-        txt = gr.Textbox(
-            show_label=False,
-            placeholder="Enter text and press enter"
-        )
-        submit_btn = gr.Button('Submit')
-        upload_btn = gr.File(label='Upload a PDF', file_types=[".pdf"])
+        with gr.Row():
+            with gr.Column(scale=1):
+                chatbot = gr.Chatbot(label='Software Architect AI', type='messages', height=680)
+                txt = gr.Textbox(
+                    show_label=False,
+                    placeholder="Enter text and press enter"
+                )
+                submit_btn = gr.Button('Submit')
+            
+            with gr.Column(scale=1):
+                pdf_display = gr.Image(label='Uploaded PDF Page', interactive=False, height=680)
+                upload_btn = gr.File(label='Upload a PDF', file_types=[".pdf"])
+    return demo
 
 if __name__ == "__main__":
-    demo.launch()
+    demo = createUI()
+    demo.launch(pwa=True, share=False)
